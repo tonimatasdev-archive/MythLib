47d6ad4ca56a15cd80473fbedc6465ba7f9fc33f
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-10-18 21:23:03 +0200

    Common screen registry

---
+++ fabric/src/main/java/dev/tonimatas/mythlib/client/fabric/RegistryClientHelpersImpl.java
@@ -0,0 +1,14 @@
+package dev.tonimatas.mythlib.client.fabric;
+
+import dev.tonimatas.mythlib.client.RegistryClientHelpers;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.MenuAccess;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+
+public class RegistryClientHelpersImpl {
+    public static <M extends AbstractContainerMenu, U extends Screen & MenuAccess<M>> void registerMenu(MenuType<? extends M> menuType, RegistryClientHelpers.ScreenConstructor<M, U> screenConstructor) {
+        MenuScreens.register(menuType, screenConstructor::create);
+    }
+}
