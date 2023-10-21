4e28fbc53629ce50b376ef1f396189b52194b18d
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-10-21 21:12:07 +0200

    Fixes

--- fabric/src/main/java/dev/tonimatas/mythlib/registry/fabric/MythRegistriesImpl.java
+++ fabric/src/main/java/dev/tonimatas/mythlib/registry/fabric/MythRegistriesImpl.java
@@ -3,8 +3,8 @@ package dev.tonimatas.mythlib.registry.fabric;
 import dev.tonimatas.mythlib.registry.MythRegistry;
 import net.minecraft.core.Registry;
 
-public interface MythRegistryImpl {
-    static <T> MythRegistry<T> create(Registry<T> registry, String id) {
+public class MythRegistriesImpl {
+    public static <T> MythRegistry<T> create(Registry<T> registry, String id) {
         return new FabricMythRegistry<>(registry, id);
     }
 }
--- forge/src/main/java/dev/tonimatas/mythlib/registry/forge/MythRegistriesImpl.java
+++ forge/src/main/java/dev/tonimatas/mythlib/registry/forge/MythRegistriesImpl.java
@@ -1,11 +1,10 @@
 package dev.tonimatas.mythlib.registry.forge;
 
 import dev.tonimatas.mythlib.registry.MythRegistry;
-import dev.tonimatas.mythlib.registry.forge.ForgeMythRegistry;
 import net.minecraft.core.Registry;
 
-public interface MythRegistryImpl {
-    static <T> MythRegistry<T> create(Registry<T> registry, String id) {
+public class MythRegistriesImpl {
+    public static <T> MythRegistry<T> create(Registry<T> registry, String id) {
         return new ForgeMythRegistry<>(registry, id);
     }
 }
